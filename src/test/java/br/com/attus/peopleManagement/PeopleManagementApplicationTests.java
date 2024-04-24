e87dbc8fbbb019ee059ec4a965846c00173d07fe
Eduardo F Zimmermann <z.edufz@gmail.com>
2024-04-23 22:12:08 -0300

    Criação projeto

---
+++ src/test/java/br/com/attus/peopleManagement/PeopleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attus.peopleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

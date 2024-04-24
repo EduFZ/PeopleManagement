4e83ac75cee043b16c422b438cd5ec6ef59349d9
Eduardo F Zimmermann <z.edufz@gmail.com>
2024-04-23 22:54:19 -0300

    feat: Criado classes entity Person e Address

---
+++ src/main/java/br/com/attus/peopleManagement/entity/Address.java
@@ -0,0 +1,29 @@
+package br.com.attus.peopleManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAddress;
+    private String street;
+    private Long cep;
+    private Long number;
+    private String city;
+    private String state;
+    private Boolean mainAddress;
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+}
---
+++ src/main/java/br/com/attus/peopleManagement/entity/Person.java
@@ -0,0 +1,27 @@
+package br.com.attus.peopleManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPerson;
+    private String name;
+    private LocalDate birth;
+    @OneToMany
+    private List<Address> address;
+
+}
